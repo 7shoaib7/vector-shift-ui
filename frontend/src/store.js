36bab572bcb6bf9878aa3b02b3a50977fd26a709
7shoaib7 <69447663+7shoaib7@users.noreply.github.com>
2024-08-01 21:57:17 +0530

    store-minor-changes

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -11,6 +11,9 @@ import {
 export const useStore = create((set, get) => ({
     nodes: [],
     edges: [],
+    nodeIDs: {},
+     
+    // Function to get a unique node ID
     getNodeID: (type) => {
         const newIDs = {...get().nodeIDs};
         if (newIDs[type] === undefined) {
@@ -18,28 +21,36 @@ export const useStore = create((set, get) => ({
         }
         newIDs[type] += 1;
         set({nodeIDs: newIDs});
+        console.log("getNodeID",`${type}-${newIDs[type]}`)
         return `${type}-${newIDs[type]}`;
     },
+    
+    // Function to add a new node
     addNode: (node) => {
         set({
             nodes: [...get().nodes, node]
         });
     },
+       
+    // Handle node and edge changes
     onNodesChange: (changes) => {
       set({
         nodes: applyNodeChanges(changes, get().nodes),
       });
     },
+    
     onEdgesChange: (changes) => {
       set({
         edges: applyEdgeChanges(changes, get().edges),
       });
     },
+    
     onConnect: (connection) => {
       set({
         edges: addEdge({...connection, type: 'smoothstep', animated: true, markerEnd: {type: MarkerType.Arrow, height: '20px', width: '20px'}}, get().edges),
       });
     },
+    
     updateNodeField: (nodeId, fieldName, fieldValue) => {
       set({
         nodes: get().nodes.map((node) => {

b7e5e2bf88c43eb08fad2b78fe27cebe1e847057
7shoaib7 <69447663+7shoaib7@users.noreply.github.com>
2024-08-01 21:58:39 +0530

    removed-console-from-store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -21,7 +21,6 @@ export const useStore = create((set, get) => ({
         }
         newIDs[type] += 1;
         set({nodeIDs: newIDs});
-        console.log("getNodeID",`${type}-${newIDs[type]}`)
         return `${type}-${newIDs[type]}`;
     },
     
